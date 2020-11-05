1ef94648afe977f28a09f8944e6dc4350dccb9cd
Emanuel D R Sena <dariosena@gmail.com>
2020-11-05 08:40:52 -0300

    Added extras for frontend exercices.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,23 +11,32 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    const transactions = await this.find();
+    const transactions: Transaction[] = await this.find();
 
-    const reducer = (accumulator: number, value: number): number =>
-      accumulator + value;
+    let income = 0;
+    let outcome = 0;
+    let total = 0;
 
-    const incomeValues = transactions.map(transaction => {
-      return transaction.type === 'income' ? transaction.value : 0;
-    });
+    if (transactions.length > 0) {
+      const reducer = (accumulator: number, value: number): number =>
+        accumulator + value;
 
-    const outcomeValues = transactions.map(transaction => {
-      return transaction.type === 'outcome' ? transaction.value : 0;
-    });
+      const incomeValues = transactions.map(transaction => {
+        return transaction.type === 'income' ? transaction.value : 0;
+      });
 
-    const income = incomeValues.reduce(reducer);
-    const outcome = outcomeValues.reduce(reducer);
+      const outcomeValues = transactions.map(transaction => {
+        return transaction.type === 'outcome' ? transaction.value : 0;
+      });
 
-    return { income, outcome, total: income - outcome };
+      income = incomeValues.reduce(reducer);
+      outcome = outcomeValues.reduce(reducer);
+      total = income - outcome;
+
+      return { income, outcome, total };
+    }
+
+    return { income, outcome, total };
   }
 }
 
